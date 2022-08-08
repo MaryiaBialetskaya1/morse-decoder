cfef463e9e81e9cd6c91768d346f0bace08df4de
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2022-08-08 16:37:54 -0400

    feat: add solution to task morse-decoder

--- src/index.js
+++ src/index.js
@@ -38,7 +38,17 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let string = [];
+    let sentence = [];
+        for (i = 0; i<expr.length; i+=10){
+            let char = expr.slice(i, i+10);
+            string.push(char);
+        }   
+        for(index in string){
+            let splited = string[index].split('').reverse().join('').replace(/01/g, '.').replace(/11/g, '-').replace(/0/g, '').split('').reverse().join('');
+            splited == '**********' ? sentence.push(' ') : sentence.push(MORSE_TABLE[splited]);
+        }
+        return sentence.join('');
 }
 
 module.exports = {
